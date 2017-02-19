42d70d40db081fe45dcb3217dcfb9bbd6a12b219
travarilo <travarilo@yahoo.com>
2017-02-20 02:20:39 +0700

    cpufreq: add interactivex governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -403,6 +403,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CRAZYACTIVE)
 extern struct cpufreq_governor cpufreq_gov_crazyactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_crazyactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor cpufreq_gov_interactivex;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
 #endif
 
 
