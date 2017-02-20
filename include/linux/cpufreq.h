7643ff8af5d710376bf1f8bc12d2f8131e733679
travarilo <travarilo@yahoo.com>
2017-02-20 11:41:28 +0700

    cpufreq: add barry_allen governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -400,6 +400,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CRAZYACTIVE)
 extern struct cpufreq_governor cpufreq_gov_crazyactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_crazyactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
+extern struct cpufreq_governor cpufreq_gov_barry_allen;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
 #endif
 
 
