f44dcb42cb88048f580ce6f798d78e160c47e244
travarilo <travarilo@yahoo.com>
2017-02-24 00:11:57 +0700

    cpufreq: add bioshock governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -406,6 +406,9 @@ extern struct cpufreq_governor cpufreq_gov_intellimm;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
+extern struct cpufreq_governor cpufreq_gov_bioshock;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_bioshock)
 #endif
 
 
