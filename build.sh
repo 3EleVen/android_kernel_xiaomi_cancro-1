eb32432b4e93434eaf812e1de3717ffd257d1f8b
Rizky Benggolo <travarilo@yahoo.com>
2017-02-28 20:36:27 +0700

    redkancut-v4.1

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 DEFCONFIG=lineageos_cancro_defconfig
 make ARCH=arm CROSS_COMPILE=~/gcc/bin/arm-eabi- $DEFCONFIG
-make ARCH=arm CROSS_COMPILE=~/gcc/bin/arm-eabi-
+make -j32 ARCH=arm CROSS_COMPILE=~/gcc/bin/arm-eabi-
 rm ~/redkancut/modules/*
 for i in `find -name *.ko`; do cp $i ~/redkancut/modules/; done
 scripts/dtbToolCM -s 2048 -d "qcom,msm-id = <" -2 -o arch/arm/boot/dt.img -p /usr/bin/ arch/arm/boot/
@@ -8,5 +8,5 @@ cp arch/arm/boot/zImage ~/redkancut/
 cp arch/arm/boot/dt.img ~/redkancut/
 cd ~/redkancut
 rm *.zip
-zip -r9 redkancut_cancro_aosp-v4.zip * -x README redkancut_cancro_aosp-v4.zip
+zip -r9 redkancut_cancro_aosp-v4.1.zip * -x README redkancut_cancro_aosp-v4.1.zip
 cp *.zip /media/sf_ubuntu/release/
